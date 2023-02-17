aac6dda91c0a40324612cab21bb00d7796b87794
ITjanari <122104148+ITjanari@users.noreply.github.com>
2023-02-17 17:23:26 +0200

    Add files via upload

---
+++ fade-in.js
@@ -0,0 +1,35 @@
+const faders = document.querySelectorAll('.fade-in-about-me'); // "About me" pealkiri
+
+const appears = document.querySelectorAll('.fade-in-about-me-text'); // "About me" alumine tekst
+
+const appearOptions = {
+    threshold: 1, 
+    rootMargin: "0px 0px -50px 0px" // Mida suurem on negatiivne arv, seda rohkem peab inimene alla liikuma, et näha animatsiooni
+};
+
+// "About me" pealkiri
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+       if (!entry.isIntersecting) {
+        return;
+       } else {
+         entry.target.classList.add('appear');
+         appearOnScroll.unobserve(entry.target);
+       }
+    })
+},
+appearOptions)
+
+
+// "About me" alumine tekst
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+appears.forEach(appear => {
+    appearOnScroll.observe(appear);
+})
---
+++ nav.js
@@ -0,0 +1,17 @@
+const navBtn = document.querySelector('#menu-btn');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelector('.nav-links');
+
+navBtn.addEventListener('click', () => {
+  navLinks.classList.add('activated');
+  const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
+  navBtn.setAttribute('aria-expanded', !isExpanded);
+  !isExpanded && nav.classList.add('active');
+})
+
+document.querySelectorAll('.nav-link').forEach
+(link => {
+  if(link.href === window.location.href){
+    link.setAttribute('aria-current', 'page')
+  }
+})
